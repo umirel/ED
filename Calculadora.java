b4d4d2aad81d5ef918bd26782c7bc7acf4524f42
Danylo <roorr123578@gmail.com>
2025-12-15 19:00:17 +0100

    2 version de calc

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,25 @@
-public class Calculadora {
+import java.util.Scanner;
 
+public class Calculadora {
     public static void main(String[] args) {
-        System.out.println(Hola Git);
+        if (args.length != 3) {
+            System.out.println("La calculadora necesita 3 parámetros");
+            System.out.println("USO: Calculadora <operación> num1 num2");
+            System.exit(-1);
+        }
+
+        String operacion = args[0];
+        double num1 = Double.parseDouble(args[1]);
+        double num2 = Double.parseDouble(args[2]);
+        
+        double resultado = switch (operacion) {
+            case "add" -> num1 + num2;
+            case "sub" -> num1 - num2;
+            case "mul" -> num1 * num2;
+            case "div" -> num1 / num2;
+            default -> throw new RuntimeException("Operación no soportada");
+        };
+
+        System.out.printf("%.2f %s %.2f = %.2f\n", num1, operacion, num2, resultado);
     }
 }
